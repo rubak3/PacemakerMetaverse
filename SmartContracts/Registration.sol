12ff04ab20e441e0ec5465df6bfb29fdd238866f
rubak3 <ruba.sleem@gmail.com>
2024-08-10 00:27:14 +0400

    First Commit

--- SmartContracts/Registration.sol
+++ SmartContracts/Registration.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NFTManager.sol";
 
 contract Registration is Ownable(msg.sender) {
 
-    NFTManager nftManagerSC;
-    uint256 public SessionID;
+    NFTManager immutable nftManagerSC;
+    uint256 public sessionId;
 
     constructor(address nftManagerSCAddr) {
         nftManagerSC = NFTManager(nftManagerSCAddr);
-        SessionID = 1;
+        sessionId = 1;
     }
 
     struct Doctor {
@@ -20,43 +20,43 @@ contract Registration is Ownable(msg.sender) {
     }
 
     struct Session {
-        string sessionURI;
+        string sessionUri;
         address patientAddr;
     }
     
-    mapping(address => Doctor) public RegisteredDoctors;
-    mapping(uint256 => Session) private Sessions;
+    mapping(address => Doctor) public registeredDoctors;
+    mapping(uint256 => Session) private sessions;
 
-    event DoctorRegistered(address Doctor, address User);
-    event SessionUploaded(address User, uint256 _SessionID);
-    event UserRequestedToRegister(address User, string UserDocuments, string DTMetadata, string DTRecords);
+    event DoctorRegistered(address doctor, address user);
+    event SessionUploaded(address user, uint256 sessionId);
+    event UserRequestedToRegister(address user, string userDocuments, string dtMetadata, string dtRecords);
 
     modifier onlyRegisteredUsers {
-        require(nftManagerSC.RegisteredUsers(msg.sender), "Only registered users can call this function");
+        require(nftManagerSC.registeredUsers(msg.sender), "Only registered users can call this function");
         _;
     }
 
-    function requestUserRegistrtion(string memory Documents, string memory DTMetadata, string memory DTRecords) public {
-        emit UserRequestedToRegister(msg.sender, Documents, DTMetadata, DTRecords);
+    function requestUserRegistration(string memory userDocuments, string memory dtMetadata, string memory dtRecords) public {
+        emit UserRequestedToRegister(msg.sender, userDocuments, dtMetadata, dtRecords);
     }
 
     function registerDoctors(address doctor) public onlyRegisteredUsers {
-        RegisteredDoctors[doctor].registered = true;
-        RegisteredDoctors[doctor].patientAddr = msg.sender;
+        registeredDoctors[doctor].registered = true;
+        registeredDoctors[doctor].patientAddr = msg.sender;
         emit DoctorRegistered(doctor, msg.sender);
     }
 
-    function UploadSession(string memory _sessionURI) public onlyRegisteredUsers {
-        Sessions[SessionID].sessionURI = _sessionURI;
-        Sessions[SessionID].patientAddr = msg.sender;
-        emit SessionUploaded(msg.sender, SessionID);
-        SessionID++;
+    function uploadSession(string memory sessionUri) public onlyRegisteredUsers {
+        sessions[sessionId].sessionUri = sessionUri;
+        sessions[sessionId].patientAddr = msg.sender;
+        emit SessionUploaded(msg.sender, sessionId);
+        sessionId++;
     }
 
-    function getSessionURI(uint256 _sessionID) public view returns (string memory) {
-        require(RegisteredDoctors[msg.sender].registered, "Only registered doctors can call this function");
-        require(RegisteredDoctors[msg.sender].patientAddr == Sessions[_sessionID].patientAddr, "Only authorized doctors can get the session URI of this patient");
-        return Sessions[_sessionID].sessionURI;
+    function getSessionURI(uint256 sessId) public view returns (string memory) {
+        require(registeredDoctors[msg.sender].registered, "Only registered doctors can call this function");
+        require(registeredDoctors[msg.sender].patientAddr == sessions[sessId].patientAddr, "Only authorized doctors can get the session URI of this patient");
+        return sessions[sessId].sessionUri;
     }
 
 }
